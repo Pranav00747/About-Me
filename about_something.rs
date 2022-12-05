3bdc45c287a3b090e11384ce80c7bb1d54c66102
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2022-12-06 00:19:28 +0530

    Create about_something.rs

---
+++ about_something.rs
@@ -0,0 +1,15 @@
+#basically i am ignoring many girls but i love one and i have a interest
+
+fn who_is_she(her_first : string, her_last : string)->boolean
+{
+  if(her_first[0]=="P" && her_last[her_last.len()-1]=="r")
+  {
+        if(her_first.len()==6 && her_last==6)
+         {
+            return true;
+         }
+  }
+  return false;
+}
+  
+            
