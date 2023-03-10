6837c510b5edbb5de2468ebc377b296ea9fb06a5
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-03-11 05:02:03 +0530

    Create something-special.rs

---
+++ something-special.rs
@@ -0,0 +1,21 @@
+struct something_me
+{
+   fn about_something(__me : string);
+}
+
+impl something_me
+{
+  fn about_something(__me : string)
+  {
+       if(__me)
+        {
+         /// I am not interested in any other things like Data Analyst, 
+         /// Human and Resource, Desktop Runner or QA anything 
+         /// my interest is in core development, product development
+         /// so stop to create any other accounts or fake things related 
+         /// to this and we are product developers and we are making products 
+         /// in single night let's think if we spend to much time on a product 
+         /// what will happen.
+        }
+   }
+}
