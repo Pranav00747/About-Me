74df98e1cc57ebd39cf3406760006cf83379998f
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-03-11 05:33:37 +0530

    Delete something-about.rs

--- something-about.rs
+++
@@ -1,7 +0,0 @@
-fn about_something_me()
-{
-/* I am not interested in any other things like Data Analyst,  Human and Resource, Desktop Runner or QA anything 
-   my interest is in core development, product development so stop to create any other accounts or fake things 
-   related to this and we are product developers and we are making products in single night let's think if we spend 
-   to much time on a product what will happen. */
-}
